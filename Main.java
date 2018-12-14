06fd0c2d2ed8632a08664ae9352fbef393b1cd6e
HealyDustin2019 <42677953+HealyDustin2019@users.noreply.github.com>
2018-12-14 10:06:12 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ public class Main
   {
     scenario1();
     scenario2();
+    scenario3();
+    scenario4();
+    scenario5();
   }
   public static void scenario1()
   {
@@ -33,7 +36,7 @@ public class Main
     public static void scenario2()
   {
 // Condition: Probability of rolling an even or greater than five, on a single 10-sided die:
-// Probability: 7/10
+// Probability: 1/6 or 16.7%
     System.out.println ("Scenario: Expected result 70%");
     int numTrue = 0;
     int numFalse = 0;
@@ -55,11 +58,81 @@ public class Main
     System.out.println ("Count of False : " + numFalse);
     System.out.println ("Condition reached " + ((float) numTrue / (float) count) * 100 + "%\n");
   }
+  public static void scenario3()
+  {
+    System.out.println ("Scenario: Expected result 25%");
+    int numTrue = 0;
+    int numFalse = 0;
+    int count = 100000;
+    for (int i = 0; i < count; i++)
+      {
+	int result1 = (int) Math.floor (Math.random () * 6) + 1;
+	int result2 = (int) Math.floor (Math.random () * 6) + 1;
+
+	if ((result1 % 2) == 0 && result2 % 2 != 0)
+	  {
+	    numTrue++;
+	  }
+	else
+	  {
+	    numFalse++;
+	  }
+      }
+    System.out.println ("Count of True : " + numTrue);
+    System.out.println ("Count of False : " + numFalse);
+    System.out.println ("Condition reached " + ((float) numTrue / (float) count) * 100 + "%\n");
+  }
+    public static void scenario4()
+  {
+    System.out.println ("Scenario: Expected result 35%");
+    int numTrue = 0;
+    int numFalse = 0;
+    int count = 100000;
+    for (int i = 0; i < count; i++)
+      {
+	int result1 = (int) Math.floor (Math.random () * 6) + 1;
+	int result2 = (int) Math.floor (Math.random () * 6) + 1;
+	int result3 = (int) Math.floor (Math.random () * 6) + 1;
+
+	if (result1 == 1 || result1 == 2 && result2 == 3 || result2 == 4 && result3 == 5 || result3 == 6)
+	  {
+	    numTrue++;
+	  }
+	else
+	  {
+	    numFalse++;
+	  }
+      }
+    System.out.println ("Count of True : " + numTrue);
+    System.out.println ("Count of False : " + numFalse);
+    System.out.println ("Condition reached " + ((float) numTrue / (float) count) * 100 + "%\n");
+  }
+   public static void scenario5()
+  {
+    System.out.println ("Scenario: Expected result 27.75%");
+    int numTrue = 0;
+    int numFalse = 0;
+    int count = 100000;
+    for (int i = 0; i < count; i++)
+      {
+	int result1 = (int) Math.floor (Math.random () * 6) + 1;
+	int result2 = (int) Math.floor (Math.random () * 6) + 1;
+	int result3 = (int) Math.floor (Math.random () * 6) + 1;
+	int result4 = (int) Math.floor (Math.random () * 6) + 1;
+
+	if (result1 != result2 && result1 != result3 && result1 != result4 && result2 != result3 && result2 != result4 && result3 != result4)
+	  {
+	    numTrue++;
+	  }
+	else
+	  {
+	    numFalse++;
+	  }
+      }
+    System.out.println ("Count of True : " + numTrue);
+    System.out.println ("Count of False : " + numFalse);
+    System.out.println ("Condition reached " + ((float) numTrue / (float) count) * 100 + "%\n");
+  }
 }
 
 
-// Output
-// Scenario: Expected result 16.7%
-// Count of True : 16507
-// Count of False : 83493
-// Condition reached 16.507%
